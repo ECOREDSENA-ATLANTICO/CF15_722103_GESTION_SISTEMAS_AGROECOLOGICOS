983ebe7542222199d1e8bdf334ed7f75312d135d
AndresDiazPinto <andresvis565@gmail.com>
2024-07-18 10:35:10 -0500

    Ajuste

--- src/config/global.js
+++ src/config/global.js
@@ -152,7 +152,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/722103_CF15_DU.pdf',
+        download: 'downloads/722103_CF15_DU.zip',
       },
       {
         icono: 'fas fa-download',
